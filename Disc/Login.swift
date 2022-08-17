ee30722822139b9a2d519bf6d5b62dbd0b3ed9e0
ex4mpl3 <ourminedev@gmail.com>
2022-08-17 16:16:28 +0500

    Onboarding view initialized

---
+++ Disc/Login.swift
@@ -0,0 +1,17 @@
+import UIKit
+import SnapKit
+
+class Login:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        styling()
+    }
+}
+
+extension Login{
+    private func styling(){
+        
+    }
+}
---
+++ Pods/Target Support Files/Pods-Disc/Pods-Disc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Disc : NSObject
+@end
+@implementation PodsDummy_Pods_Disc
+@end
